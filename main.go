b9f961484f32caed0bcf3d9fb171e6358ef76534
Apostolis Mpostanis <a.mpostanis@logicea.com>
2019-03-14 15:01:23 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	// w.Write("hello World")
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", sayHello)
+	http.ListenAndServe("8080", nil)
+}
